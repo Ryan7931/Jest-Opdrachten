0ea05d3dc2d35110136d19e48e5ed0483dbbf913
Ryan7931 <9024915@student.zadkine.nl>
2026-05-21 10:09:23 +0200

    jest opdracht 9

--- src/opdracht-09/calculator.js
+++ src/opdracht-09/calculator.js
@@ -8,23 +8,25 @@
  */
 
 export function add(a, b) {
-  // TODO: Implementeer deze functie
+  return a + b;
 }
 
 export function subtract(a, b) {
-  // TODO: Implementeer deze functie
+  return a - b;
 }
 
 export function multiply(a, b) {
-  // TODO: Implementeer deze functie
+  return a * b;
 }
 
 export function divide(a, b) {
-  // TODO: Implementeer deze functie
-  // Let op: gooi een error bij delen door 0!
+  if (b === 0) {
+    throw new Error('Cannot divide by zero');
+  }
+  return a / b;
 }
 
 export function power(base, exponent) {
-  // TODO: Implementeer deze functie
-  // Tip: gebruik Math.pow() of ** operator
+  return Math.pow(base, exponent);
+  // Of: return base ** exponent;
 }
